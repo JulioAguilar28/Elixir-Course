f4d18ba89456cd0eaa0113ac314cd4362bba3f6d
Julio Aguilar <julio.aguilar9928@gmail.com>
2021-02-01 20:42:58 -0600

    🚀 count_binary function was defined

--- counter/lib/counter.ex
+++ counter/lib/counter.ex
@@ -15,4 +15,20 @@ defmodule Counter do
   def hello do
     :world
   end
+
+  def count_lines(file_name \\ "words") do
+    num_lines =
+      File.read!(file_name)
+      |> String.split()
+      |> Enum.count()
+
+    {:ok, num_lines}
+  end
+
+  def count_binary(file_name) do
+    content = File.read!(file_name)
+    hash = :crypto.hash(:md5, content)
+
+    {:ok, byte_size(hash)}
+  end
 end
